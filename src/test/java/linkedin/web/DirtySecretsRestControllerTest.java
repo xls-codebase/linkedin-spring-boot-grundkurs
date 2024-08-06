3b8a651c28a6ab5925a7a61c44705441da7c9806
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-08-06 15:52:54 +0200

    bugfix

--- src/test/java/linkedin/web/DirtySecretsRestControllerTest.java
+++ src/test/java/linkedin/web/DirtySecretsRestControllerTest.java
@@ -1,24 +1,19 @@
 package linkedin.web;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.UUID;
+
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyCollection;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 
 @WebMvcTest(DirtySecretsRestController.class)
@@ -34,9 +29,10 @@ class DirtySecretsRestControllerTest {
     @Test
     void shouldSaveSecret() throws Exception  {
         DirtySecret secret = mock(DirtySecret.class);
+        UUID testId = UUID.randomUUID();
         Mockito.when(repository.save(any(DirtySecret.class)))
                 .thenReturn(secret);
-        Mockito.when(secret.getId()).thenReturn("test");
+        Mockito.when(secret.getId()).thenReturn(testId);
         mockMvc
             .perform(MockMvcRequestBuilders.post("/dirty-secrets")
                     .contentType(MediaType.APPLICATION_JSON)
