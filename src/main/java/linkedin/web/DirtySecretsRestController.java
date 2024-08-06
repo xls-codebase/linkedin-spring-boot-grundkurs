5547bfffc45f322224a2c7bbc09595f55599ec61
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-08-06 15:50:27 +0200

    added database-support with Spring Data & JPA

--- src/main/java/linkedin/web/DirtySecretsRestController.java
+++ src/main/java/linkedin/web/DirtySecretsRestController.java
@@ -1,9 +1,10 @@
 package linkedin.web;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/dirty-secrets")
@@ -15,46 +16,20 @@ public class DirtySecretsRestController {
         this.repository = repository;
     }
 
-    @GetMapping("/count")
-    public int count() {
-        return this.repository.count();
+    @GetMapping
+    public Iterable<DirtySecret> get() {
+        return this.repository.findAll();
     }
 
-    //  Variante 1
-    // Die Spring ResponseStatusException-Klasse
-    // new ResponseStatusException(httpStatus.NOT_FOUND)
-    @GetMapping("/e1/{id}")
-    public DirtySecret getByIdE1(@PathVariable("id") String id) {
-        return this.repository.getById(id)
-                .orElseThrow(() -> new ResponseStatusException(
-                        HttpStatus.NOT_FOUND,
-                        "No secret found"
-                ));
+    @GetMapping("/{id}")
+    public DirtySecret getById(@PathVariable String id) {
+        return this.repository.findById(UUID.fromString(id))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Found nothing."));
     }
 
-    //  Variante 2
-    // Die Annotation @ResponseStatus an Exception-Klasse
-    // @ResponseStatus(value=HttpStatus.NOT_FOUND)
-    @GetMapping("/e2/{id}")
-    public DirtySecret getByIdE2(@PathVariable("id") String id) {
-        return this.repository.getById(id)
-                .orElseThrow(() -> new NoSecretFoundWebException());
-    }
-
-    //  Variante 3
-    // eigene Exception-Handler-Methode mit @ExceptionHandler
-    @GetMapping("/e3/{id}")
-    public DirtySecret getByIdE3(@PathVariable("id") String id) {
-        return this.repository.getById(id)
-                .orElseThrow(() -> new NoSecretFoundException());
-    }
-
-    @ExceptionHandler({NoSecretFoundException.class})
-    public ResponseEntity<String> handleNoSecretFoundException() {
-        return ResponseEntity.internalServerError().body("No secret found.");
-    }
     @PostMapping
     public DirtySecret post(@RequestBody DirtySecret secret) {
-        return this.repository.save(secret);
+        var savedSecret = this.repository.save(secret);
+        return savedSecret;
     }
 }
